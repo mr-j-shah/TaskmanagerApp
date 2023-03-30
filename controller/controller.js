02291b4ebf373eeae8a4b80655e6b767e0aa97e6
mr-j-shah <shahjinay02@gmail.com>
2023-03-30 11:13:12 +0530

    Task Manager Api Completed

--- controller/controller.js
+++ controller/controller.js
@@ -1,32 +1,86 @@
 const conn = require("../db/connection")
 
 const getAllTask=(req,res)=>{
-    
-    res.send("getAllTaask");
+    const { id: userID } = req.params;
+    var sql = "SELECT * FROM tasktable WHERE userid = ?";
+    conn.query(sql,userID,function (err,result,fields) {
+        if (err) {
+            res.json({ "Status": "Error","message":err });
+            res.end(); 
+        }
+        else{
+            var string=JSON.stringify(result);
+            var json =  JSON.parse(string); 
+            if (json.length==0) {
+                res.json({"Status":"NoDataFound","Result":{}});
+                res.end();
+            } 
+            else{
+                res.json({"Status":"Success","Result":json});
+                res.end();
+            }
+        }
+    });
 }
 
-const updateTask=(req,res)=>{
-    
+const updateTask=(req,res)=>{    
     const { id: taskID } = req.params
     res.send(taskID+" Update Task");
 }
 
 const deleteTask=(req,res)=>{
-    
-    const { id: taskID } = req.params
-    res.send(taskID+" Delete Task");
+    const { id: taskID } = req.params;
+    var sql = "DELETE FROM `tasktable` WHERE id=?";
+    conn.query(sql,taskID,function (err,result,fields) {
+        
+        if (err) {
+            res.json({ "Status": "Error","message":err });
+            res.end(); 
+        }
+        else{
+            res.json({"Status":"Success","Result":result});
+            res.end();
+        }
+    });
 }
 
 const getTask=(req,res)=>{
-    
-    const { id: taskID } = req.params
-    res.send(taskID+" Get Task");
+    const { id: taskID } = req.params;
+    var sql = "SELECT * FROM `tasktable` WHERE id=?";
+    conn.query(sql,taskID,function (err,result,fields) {
+        if (err) {
+            res.json({ "Status": "Error","message":err });
+            res.end(); 
+        }
+        else{
+            var string=JSON.stringify(result);
+            var json =  JSON.parse(string); 
+            if (json.length==0) {
+                res.json({"Status":"NoDataFound","Result":{}});
+                res.end();
+            } 
+            else{
+                res.json({"Status":"Success","Result":json});
+                res.end();
+            }
+        }
+    });
 }
 
 const creatTask=async (req,res)=>{
-    
-    const {email} = req.body;
-    console.log(email);
-    res.json(req.body)
+    const {userid,taskname,taskdetails,datetime} = req.body;
+    var done = false;
+    var values = [[userid,taskname,taskdetails,done,datetime]];
+    var sql = "INSERT INTO tasktable(userid, name, details, done, time) VALUES (?)";
+    conn.query(sql,values,function (err, result, fields) {  
+        if (err) {
+            res.json({ "Status": "Error","message":err });
+            res.end(); 
+        }
+        else{
+            res.json({ "Status": "Sucess","message":result });
+            res.end();
+        }
+    });
 }
 module.exports = {creatTask,getAllTask,getTask,deleteTask,updateTask}
--- router/rouer.js
+++ router/rouer.js
@@ -1,9 +1,10 @@
 const express = require("express"); 
 const router = express.Router();
 
-const { updateTask, deleteTask, getAllTask, getTask ,creatTask } = require("../controller/controller");
+const { updateTask,getAllTask, deleteTask, getTask ,creatTask } = require("../controller/controller");
 
-router.route("/").get(getAllTask).post(creatTask);
+router.route("/").post(creatTask);
 router.route("/:id").patch(updateTask).get(getTask).delete(deleteTask);
+router.route("/userid/:id").get(getAllTask);
 
 module.exports = router;
