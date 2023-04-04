76b48ecbdb9a7c3a674e7c79faae823926afbc8c
mr-j-shah <shahjinay02@gmail.com>
2023-04-04 10:32:31 +0530

    Confirm router added

--- app.js
+++ app.js
@@ -3,24 +3,11 @@ const app = express();
 
 const controllerRouter = require("./router/rouer");
 const authenticationRouter = require("./router/login_router");
-const conn = require("./db/connection")
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use("/api/v1/tasks",express.urlencoded({extended:true}),controllerRouter);
 app.use("/login",express.urlencoded({extended:true}),authenticationRouter);
-app.get("/confirm",function(req,res){
-    const {tocken:tocken} = req.query;
-    var sql = "UPDATE `login` SET `confirm`=TRUE WHERE tocken=?";
-    conn.query(sql,tocken,function (err,result,fields) {
-        if (err) {
-            res.json({ "Status": "Error","message":err});
-            res.end(); 
-        }
-        else{
-            res.json({"Status":"Success","Result":result});
-            res.end();
-        }
-    });
-});
+
 const port = process.env.PORT || 2000; 
 app.listen(port);
--- router/login_router.js
+++ router/login_router.js
@@ -4,8 +4,20 @@ const fs = require("fs");
 
 const {login} = require("../authentication/login");
 const {signup} = require("../authentication/signup");
-
+const conn = require("../db/connection")
 router.route("/").post(login);
 router.route("/signup").post(signup);
-
+router.route("/confirm").get(function(req,res){
+    const {tocken:tocken} = req.query;
+    var sql = "UPDATE `login` SET `confirm`=TRUE WHERE tocken=?";
+    conn.query(sql,tocken,function (err,result,fields) {
+        if (err) {
+            res.send("<h1>Error in Confirmation</h1>")
+            res.end(); 
+        }
+        else{
+            res.send("<h1>Sucessfully Registered Email</h1><h3>Go to Login</h3>")
+            res.end();
+        }
+    })});
 module.exports = router;
